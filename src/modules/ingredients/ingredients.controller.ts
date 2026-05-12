1108c63610a8d1cfcd5eaac4e92c8632656775ec
LihuenCortiana <lihuen.albores.cortiana@gmail.com>
2026-05-12 04:58:35 -0300

    fuera comentarios

--- src/modules/ingredients/ingredients.controller.ts
+++ src/modules/ingredients/ingredients.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
 import { Ingredient } from "./entities/ingedients.entity";
 import { IngredientsRepository } from "./ingredients.repository";
 import { createIngredientDto } from "./DTOs/ingredientDto";
-//import { Roles } from "../auth/decorators/roles.decorator";
-//import { RolesGuard } from "../auth/guards/roles.guard";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { RolesGuard } from "../auth/guards/roles.guard";
 
 @Controller('ingredients')
 
@@ -16,8 +16,8 @@ export class IngredientController{
     }
 
     @Post()
-    //@UseGuards(RolesGuard)
-    //@Roles(1)
+    @UseGuards(RolesGuard)
+    @Roles(1)
     public async postIngredientes(@Body() createDto: createIngredientDto): Promise <Ingredient>{
         return await this.ingredientsRepository.crearIngredientes(createDto)
     }
