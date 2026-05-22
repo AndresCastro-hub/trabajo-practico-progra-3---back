92d8eefcf199c5aeeb3d3ed5919cea272830ed7b
Andres Castro <andrescastro2011@hotmail.com>
2026-05-22 17:17:43 -0300

    refactor para quitar acoplamiento

--- src/modules/fatsecret/fatsecret.module.ts
+++ src/modules/fatsecret/fatsecret.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FatSecretService } from './fatsecret.service';
 import { NUTRITION_SERVICE } from '../nutrition/nutrition.interface';
+import { TranslationModule } from '../translation/translation.module';
 
 @Module({
+    imports: [TranslationModule],
     providers: [
         {
             provide: NUTRITION_SERVICE,
