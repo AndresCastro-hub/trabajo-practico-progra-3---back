2f6bad151e15c58480ef8920090d54aa6f039dbc
Andres Castro <andrescastro2011@hotmail.com>
2026-06-08 20:42:48 -0300

    endpoint para obtener las recetas del calendario

--- src/modules/calendar/calendar.module.ts
+++ src/modules/calendar/calendar.module.ts
@@ -5,10 +5,11 @@ import { Recipe } from '../recipes/entities/recipe.entity';
 import { CalendarController } from './calendar.controller';
 import { CalendarService } from './calendar.service';
 import { TipoComida } from './entities/tipo-comida-entity';
+import { CalendarWeekService } from './calendar-week.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Calendar, Recipe, TipoComida])],
     controllers: [CalendarController],
-    providers: [CalendarService]
+    providers: [CalendarService, CalendarWeekService]
 })
 export class CalendarModule {}
